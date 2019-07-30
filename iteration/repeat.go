c4d9b2c6d1e73850d4fc10c8cf846afdfca66484
Christopher Stöckl <christopherstoeckl1@gmail.com>
2019-07-30 23:46:53 +0200

    Repeat function now accepts argument for how many times to repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,7 @@
 package iteration
 
-const repeatCount = 5
-
 // Repeat repeats a given character 5 times
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 	var repeated string
 	for i := 0; i < repeatCount; i++ {
 		repeated += character
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,10 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 6)
+	expected := "aaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -12,7 +15,11 @@ func TestRepeat(t *testing.T) {
 }
 
 func BenchmarkRepeat(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		Repeat("a")
-	}
+	Repeat("a", b.N)
+}
+
+func ExampleRepeat() {
+	repeatedString := Repeat("a", 6)
+	fmt.Println(repeatedString)
+	// Output: aaaaaa
 }
