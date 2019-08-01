607953c9da459fd9b055e525aa53e7a963557330
Christopher Stöckl <christopherstoeckl1@gmail.com>
2019-08-01 18:02:35 +0200

    Arrays and slices: Add SumAll function with tests

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,6 +1,6 @@
 package main
 
-// Sum takes an array of 5 numbers and returns the sum
+// Sum takes a slice of numbers and returns the sum
 func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
@@ -8,4 +8,16 @@ func Sum(numbers []int) int {
 	}
 
 	return sum
+}
+
+// SumAll takes multiple slices and returns a slice with the sum of their values
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
